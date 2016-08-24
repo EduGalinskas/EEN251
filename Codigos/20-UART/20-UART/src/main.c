2115ac54bb73d7e37f390c8f725fec187f1ec4d4
Corsi <corsiferrao@gmail.com  git config --global user.name Your>
2016-08-24 14:47:57 -0300

    aula 20

---
+++ Codigos/20-UART/20-UART/src/main.c
@@ -0,0 +1,98 @@
+/**
+ *	20-UART 
+ * Prof. Rafael Corsi
+ *
+ *    (e.g., HyperTerminal on Microsoft Windows) with these settings:
+ *   - 115200 bauds
+ *   - 8 bits of data
+ *   - No parity
+ *   - 1 stop bit
+ *   - No flow control
+ */
+
+#include "asf.h"
+#include "conf_board.h"
+#include "conf_clock.h"
+
+/************************************************************************/
+/* Configurações                                                        */
+/************************************************************************/
+
+#define STRING_EOL    "\r"
+#define STRING_VERSAO "-- "BOARD_NAME" --\r\n" \
+					  "-- Compiled: "__DATE__" "__TIME__" --"STRING_EOL
+
+#define CONF_UART_BAUDRATE 115200		
+#define CONF_UART          CONSOLE_UART
+
+/************************************************************************/
+/* Configura UART                                                       */
+/************************************************************************/
+void config_uart(void){
+	
+	/* configura pinos */
+	gpio_configure_group(PINS_UART0_PIO, PINS_UART0, PINS_UART0_FLAGS);
+	
+	/* ativa clock */
+	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+	
+	/* Configuração UART */
+	const usart_serial_options_t uart_serial_options = {
+		.baudrate   = CONF_UART_BAUDRATE,
+		.paritytype = UART_MR_PAR_NO,
+		.stopbits   = 0
+	};
+	
+	stdio_serial_init((Usart *)CONF_UART, &uart_serial_options);
+}
+
+/************************************************************************/
+/* Display Menu                                                         */
+/************************************************************************/
+static void display_menu(void)
+{
+	puts(" 1 : exibe novamente esse menu \n\r"
+		 " 2 : Ativa o LED  \n\r"
+		 " 3 : Desliga o LED \n\r ");
+}
+
+/************************************************************************/
+/* Main                                                                 */
+/************************************************************************/
+int main(void)
+{
+	uint8_t uc_key;
+
+	/* Initialize the system */
+	sysclk_init();
+	board_init();
+
+	/* Initialize debug console */
+	config_uart();
+	
+	/* frase de boas vindas */
+	puts(" ---------------------------- \n\r"
+	 	 " Bem vindo terraquio !		\n\r"
+		 " ---------------------------- \n\r");
+		 
+	/* display main menu */
+	display_menu();
+
+	while (1) {
+		usart_serial_getchar((Usart *)CONSOLE_UART, &uc_key);	
+		switch (uc_key) {
+			case '1':
+				display_menu();
+				break;
+			case '2':
+				LED_On(0);
+				puts("Led ON \n\r");
+				break;
+			case '3' :
+				LED_Off(0);
+				puts("Led OFF \n\r");
+			default:
+				printf("Opcao nao definida: %d \n\r", uc_key);
+		}	
+	}
+}
---
+++ Pesquisa e Problemas/18 - Projeto NXP/README.txt
@@ -0,0 +1,18 @@
+Campos
+
+
+Nome do Projeto (campo obrigatório)*
+
+Descrição do Projeto (campo obrigatório)*
+faça uma descrição do seu projeto com no mínimo 300 palavras.
+
+Diferenciais da Solução (campo obrigatório)*
+faça uma descrição dos diferenciais da solução com no mínimo 100 palavras.
+
+Pontos Críticos (campo obrigatório)*
+faça uma descrição dos pontos críticos com no mínimo 100 palavras.
+
+Diagrama de Blocos Funcional da Solução (em PDF) com até 4 imagens (campo obrigatório) *
+
+Insumos Adicionais Necessários (campo obrigatório)*
+máximo 300 palavras.
